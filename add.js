787d908a5914a86538234c9f54b795990fad6e7c
lilia <lilianom59@outlook.fr>
2022-12-02 07:58:26 +0100

    site

---
+++ add.js
@@ -0,0 +1,12 @@
+
+//                 --------- PROJET-PERSO : E-Commerce --------
+
+let menu = document.getElementById('menuIcon')
+let burgerMenu = document.getElementById('navigation')
+
+menu.addEventListener('click', function(){
+        burgerMenu.style.visibility = "visible";
+        burgerMenu.style.animationName = "animMenu"
+        burgerMenu.style.animationDuration = "400ms";
+})
+
